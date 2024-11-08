0e780e4bfb96e877c9ae318e4e3a72e67eac4196
jinh5 <84804779+jinh5@users.noreply.github.com>
2024-11-07 21:14:38 -0500

    updated createlist command
    Updated create list command to reflect new schema and renamed lists cog to filmsandlists

--- cogs/filmsandlists.py
+++ cogs/filmsandlists.py
@@ -6,8 +6,8 @@ from discord.ext import commands
 
 load_dotenv()
 
-class Lists(commands.Cog):
-  """Commands for organizing lists"""
+class Films_And_Lists(commands.Cog):
+  """Commands for organizing films & lists"""
   def __init__(self, client):
     self.client = client
   
@@ -19,13 +19,13 @@ class Lists(commands.Cog):
   async def createlist(self, interaction: discord.Interaction, name: str):
     join_name = ''.join(name)
     embed_message = discord.Embed()
-    command = 'CREATE TABLE ' + join_name + ' (film_id INT, title TEXT, CONSTRAINT fk_film_id FOREIGN KEY (film_id) REFERENCES master_list(film_id), CONSTRAINT fk_title FOREIGN KEY (title) REFERENCES master_list(title))' 
-    await self.client.db.execute(command)
-    command2 = 'ALTER TABLE ' + join_name + ' ENABLE ROW LEVEL SECURITY'
-    await self.client.db.execute(command2)
+    await self.client.db.execute(
+      'INSERT INTO lists(list_name) VALUES ($1)',
+      join_name
+    )
     embed_message.add_field(name=join_name, value='has been created')
     await interaction.response.send_message(embed=embed_message)
       
 
 async def setup(client):
-  await client.add_cog(Lists(client), guilds=[discord.Object(id=os.getenv('SERVER_ID'))])
+  await client.add_cog(Films_And_Lists(client), guilds=[discord.Object(id=os.getenv('SERVER_ID'))])
