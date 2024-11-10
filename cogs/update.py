614799b649cbe309209f6d93a50e3fa9966a5fde
jinh5 <84804779+jinh5@users.noreply.github.com>
2024-11-10 18:06:11 -0500

    added renamelist command

--- cogs/update.py
+++ cogs/update.py
@@ -25,5 +25,21 @@ class Update(commands.Cog):
     await interaction.response.send_message(embed=embed_message)
   '''
 
+  @app_commands.command(name='renamelist', description='Change the name of a list')
+  async def renamelist(self, interaction: discord.Interaction, originalname: str, newname: str):
+    originalname_str = ''.join(originalname)
+    newname_str = ''.join(newname)
+    await self.client.db.execute(
+      '''
+      UPDATE lists
+      SET list_name = ($1)
+      WHERE list_name = ($2)
+      ''',
+      newname_str, originalname_str
+    )
+    embed_message = discord.Embed()
+    embed_message.add_field(name='', value='**'+originalname_str+'** has been renamed to **'+newname_str+'**')
+    await interaction.response.send_message(embed=embed_message)
+
 async def setup(client):
   await client.add_cog(Update(client), guilds=[discord.Object(id=os.getenv('SERVER_ID'))])
