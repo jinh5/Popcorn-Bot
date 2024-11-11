883e696f62ab2ea619b864b962b23f34717df8f6
jinh5 <84804779+jinh5@users.noreply.github.com>
2024-11-10 19:10:41 -0500

    edited a few command inputs in help
    Some of the commands in the help command had inputs names that were inconsistent with the implemented input variable names

--- cogs/misc.py
+++ cogs/misc.py
@@ -23,8 +23,8 @@ class Misc(commands.Cog):
   async def help(self, interaction: discord.Interaction):
     embed_message = discord.Embed(title='Help Desk for Popcorn Bot', description='List of all commands for Popcorn Bot. Note: all entries will be documented in a master list containing everything.')
     embed_message.add_field(name='', value='', inline=False)
-    embed_message.add_field(name='createlist [list name]', value='Create a new list', inline=False)
-    embed_message.add_field(name='addfilm [film title]', value='Adds a film to the master list', inline=False)
+    embed_message.add_field(name='createlist [name]', value='Create a new list', inline=False)
+    embed_message.add_field(name='addfilm [title]', value='Adds a film to the master list', inline=False)
     embed_message.add_field(name='add [film title] [list name]', value='Add a film to the specified list', inline=False)
     embed_message.add_field(name='viewlists', value='View all the lists that have been created', inline=False)
     embed_message.add_field(name='viewlist [list name]', value='View all films in the specified list', inline=False)
